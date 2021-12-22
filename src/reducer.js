2dd6468f265d0d1e1996b83a4f877bf98565758d
yulia tertyshnyk <yulia.tertyshnyk@gmail.com>
2021-12-22 16:30:19 +0200

    added reducer function

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,20 @@
 function reducer(input, action) {
-  // Ваше решение тут
-  test
+  switch (action) {
+    case "upperCase":
+      string.toUpperCase();
+      break;
+    case "lowerCase":
+      string.toLowerCase();
+      break;
+    case "capitalize":
+      str = string.split(" ");
+      newStr = str.map((item) => {
+        let firstSym = item[0].toUpperCase();
+        return firstSym + item.slice(1);
+      });
+      return newStr.join(" ");
+      break;
+  }
 }
 
 module.exports = reducer;
